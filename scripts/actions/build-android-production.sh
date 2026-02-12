be5a9cd597e9e832685d6b1e4741feb99331ebed
Noel Georgi <noelmcv7@gmail.com>
2026-02-12 14:40:44 +0530

    caching

--- scripts/actions/build-android-production.sh
+++ scripts/actions/build-android-production.sh
@@ -4,7 +4,6 @@ set -euo pipefail
 mkdir -p "${RUNNER_TEMP}/artifacts"
 bunx eas build --local \
   --non-interactive \
-  --clear-cache \
   --output="${RUNNER_TEMP}/artifacts/app-production.aab" \
   --platform=android \
   --profile=production
